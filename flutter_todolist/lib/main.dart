8f25ea1acc89368ec92ec090d08f2af6cd389751
desulg <31885461+desulg@users.noreply.github.com>
2019-04-15 17:03:54 +0300

    Update main.dart

--- flutter_todolist/lib/main.dart
+++ flutter_todolist/lib/main.dart
@@ -2,44 +2,6 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(new MyApp(
-    title: new Text("MyApp"),
-    someText: new Text("Some text"),
-
+    
   ));
-
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({this.title, this.someText});
-  final Widget title, someText;
-  int counter = 0;
-  bool chkValue = false;
-
-  @override
-  Widget build (BuildContext ctxt) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: this.title,
-        ),
-        body: new Column(
-          children: <Widget>[
-            this.someText,
-            new Text("Counter value => $counter"),
-            new FlatButton(
-                onPressed: () {
-                  counter++;
-                },
-                child: new Text("AButton")
-            ),
-            new Checkbox(
-                value: chkValue,
-                onChanged: (bool newValue) {
-                  chkValue = newValue;
-                })
-          ],
-        )
-      ),
-    );
-  }
 }
