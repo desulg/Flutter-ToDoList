68124aacb4f17a8389ea053763a6222ea73051cc
desulg <31885461+desulg@users.noreply.github.com>
2019-04-15 20:13:52 +0300

    functionality works

--- flutter_todolist/lib/main.dart
+++ flutter_todolist/lib/main.dart
@@ -8,65 +8,105 @@ void main(){
 }
 
 class MyApp extends StatefulWidget {
-
   MyApp({this.title, this.someText});
   final Widget title, someText;
   @override
   MyAppState createState() => new MyAppState();
 }
-enum TestEnum { A }
 class MyAppState extends State<MyApp> {
-  String text = "";
-  @override
-  Widget build(BuildContext ctxt){
+  TextEditingController eCtrl = new TextEditingController();
+  bool showDialog = false;
+  List<String> textList = [];
+  List<bool> textChkBox = [];
+
+  Widget build(BuildContext ctxt) {
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: widget.title,
-          backgroundColor: Colors.orange,
+            title: widget.title,
           actions: <Widget>[
             new IconButton(
                 icon: new Icon(Icons.add_comment),
                 onPressed: () {
                   setState(() {
-                    text="Add comment";
+                    showDialog = true;
                   });
                 }
-            ),
+              ),
             new IconButton(
                 icon: new Icon(Icons.remove),
                 onPressed: () {
+                  int counter = 0;
+                  while(counter < textList.length){
+                    if(textChkBox[counter] == true){
+                      textChkBox.removeAt(counter);
+                      textList.removeAt(counter);
+                      counter = 0;
+                    } else {
+                      counter++;
+                    }
+                  }
                   setState(() {
-                    text="Remove comment";
+
                   });
                 }
-            ),
-            new PopupMenuButton(
-                itemBuilder: (BuildContext ctxt){
-                  return <PopupMenuEntry<TestEnum>>[
-                    new PopupMenuItem(
-                        child: new FlatButton(
-                            onPressed: (){
-                              setState(() {
-                                text = "Popup";
-                              });
-                            },
-                            child: new Text("Button")
-                        )
-                    )
-                  ];
-                  }
-                )
+            )
           ],
         ),
+
         body: new Column(
           children: <Widget>[
-            widget.someText,
-            new Text("Text Value => $text")
+            new Text("Hello flutter"),
+            showDialog ==  true?
+            new AlertDialog(
+              title: new Text("Alert dialog"),
+              content: new TextField(
+                controller: eCtrl,
+                decoration: new InputDecoration.collapsed(hintText: "ADD XYZ"),
+                maxLines: 3,
+                onSubmitted: (String text) {
+
+
+                },
+              ),
+              actions: <Widget>[
+                new FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        showDialog =  false;
+                        textList.add(eCtrl.text);
+                        textChkBox.add(false);
+                        eCtrl.clear();
+                      });
+                    },
+                    child: new Text("OK")
+                )
+              ],
+            ) : new Text(""),
+            new Flexible(
+              child: new ListView.builder(
+                itemCount: textList.length,
+                itemBuilder: (BuildContext ctxt, int index){
+                  return new Row(
+                    children: <Widget>[
+                      new Checkbox(
+                        value: textChkBox[index],
+                        onChanged: (bool newValue){
+                          textChkBox[index] = newValue;
+                          setState(() {
+
+                          });
+                        },
+                      ),
+                      new Text(textList[index])
+                    ],
+                  );
+                }
+              )
+            )
           ],
         ),
       ),
     );
-
   }
 }
