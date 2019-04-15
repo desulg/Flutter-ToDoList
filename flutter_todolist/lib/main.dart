c91793c67cd512e3549443f92a3cd998a7f9a660
desulg <31885461+desulg@users.noreply.github.com>
2019-04-15 16:37:39 +0300

    this.firstline and secondline

--- flutter_todolist/lib/main.dart
+++ flutter_todolist/lib/main.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(new MyApp());
+  runApp(new MyApp(
+    firstLine: new Text("First Line"),
+    secondLine: new Text("second line"),
+  ));
 
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({this.firstLine, this.secondLine});
+  final Widget firstLine, secondLine;
 
   @override
-  Widget build (BuildContext context){
-    return new Directionality(
-        textDirection: TextDirection.ltr,
-        child: new Center(
-            child: new Text("hello flutter")
+  Widget build (BuildContext ctxt){
+    return new MaterialApp(
+      title:"MyApplication",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("My App"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            this.firstLine,
+            this.secondLine,
+          ],
         )
+      )
     );
   }
 }
